61a846fed1f3bd6ce5c9824d36fdf017e4c432d9
Zain Fathoni <zain.fathoni@gmail.com>
2018-11-27 07:46:29 +0800

    42 - Removing Cart Items

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -239,6 +239,32 @@ const Mutations = {
       },
       info
     )
+  },
+  async removeFromCart(parent, args, ctx, info) {
+    // 1. Find the cart item
+    const cartItem = await ctx.db.query.cartItem(
+      {
+        where: {
+          id: args.id
+        }
+      },
+      `{ id, user { id} }`
+    )
+    // 2. Make sure we found an item
+    if (!cartItem) throw new Error('No CartItem Found!')
+    // 3. Make sure they own that cart item
+    if (cartItem.user.id !== ctx.request.userId) {
+      throw new Error('Cheatin huhhhh')
+    }
+    // 4. Delete that cart item
+    return ctx.db.mutation.deleteCartItem(
+      {
+        where: {
+          id: args.id
+        }
+      },
+      info
+    )
   }
 }
 
