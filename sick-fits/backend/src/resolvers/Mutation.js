38d5f998928efddfa725089846e8117e88ecbf28
Zain Fathoni <zain.fathoni@gmail.com>
2018-12-07 23:59:54 +0800

    51 - Saving Orders to the Database

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -281,7 +281,7 @@ const Mutations = {
       cart {
         id
         quantity
-        item { title price id description image }
+        item { title price id description image largeImage }
       }}`
     )
     // 2. Recalculate the total for the price
@@ -297,9 +297,33 @@ const Mutations = {
       source: args.token
     })
     // 4. Convert the CartItems to OrderItems
+    const orderItems = user.cart.map(cartItem => {
+      const orderItem = {
+        ...cartItem.item,
+        quantity: cartItem.quantity,
+        user: { connect: { id: userId } }
+      }
+      delete orderItem.id
+      return orderItem
+    })
     // 5. Create the Order
-    // 6. Clean up - Clear the users Cart, delet CartItems
+    const order = await ctx.db.mutation.createOrder({
+      data: {
+        total: charge.amount,
+        charge: charge.id,
+        items: { create: orderItems },
+        user: { connect: { id: userId } }
+      }
+    })
+    // 6. Clean up - Clear the users Cart, delete CartItems
+    const cartItemIds = user.cart.map(cartItem => cartItem.id)
+    await ctx.db.mutation.deleteManyCartItems({
+      where: {
+        id_in: cartItemIds
+      }
+    })
     // 7. Return the Order to the client
+    return order
   }
 }
 
--- sick-fits/frontend/components/TakeMyMoney.js
+++ sick-fits/frontend/components/TakeMyMoney.js
@@ -28,14 +28,17 @@ function totalItems(cart) {
 }
 
 class TakeMyMoney extends React.Component {
-  onToken = (res, createOrder) => {
+  onToken = async (res, createOrder) => {
     console.log(res.id)
     // Manually call the mutation once we have the stripe token
-    createOrder({
+    const order = await createOrder({
       variables: {
         token: res.id
       }
+    }).catch(err => {
+      alert(err.message)
     })
+    console.log(order)
   }
   render() {
     return (
@@ -50,7 +53,9 @@ class TakeMyMoney extends React.Component {
                 amount={calcTotalPrice(me.cart)}
                 name="Sick Fits"
                 description={`Order of ${totalItems(me.cart)} items`}
-                image={me.cart[0].item && me.cart[0].item.image}
+                image={
+                  me.cart.length && me.cart[0].item && me.cart[0].item.image
+                }
                 stripeKey="pk_test_X7m9nJSGV6ERc0lsNAkpySOI"
                 currency="USD"
                 email={me.email}
